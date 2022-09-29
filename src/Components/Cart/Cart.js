bd88d43df3a7abdf4edc19b7951c4dd17f3f4643
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2022-09-29 08:55:06 +0600

    Personal details Added

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -4,7 +4,11 @@ import './Cart.css'
 const Cart = () => {
     return (
         <div className='cart'>
-            <h2>Kaiser Tanveer</h2>
+            <div className="personal-info">
+            <h2><img src="./image/MyAbs.jpg" alt="" />  Kaiser Tanveer</h2>
+            <p style={{textAlign: 'center'}}>From: Kanchana, Satkania, Chattogram</p>
+            <hr style={{width: '70%'}}/>
+            </div>
         </div>
     );
 };
