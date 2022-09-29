5d34925cf5472fb76029a227eed1f7a4900ad215
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2022-09-29 10:12:50 +0600

    Cart design Completed

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -9,6 +9,26 @@ const Cart = () => {
             <p style={{textAlign: 'center'}}>From: Kanchana, Satkania, Chattogram</p>
             <hr style={{width: '70%'}}/>
             </div>
+            <div className="rest">
+                <h2>Rest Time</h2>
+                <div className="btn-group">
+                    <button className='rest-btn'><span>20</span>s</button>
+                    <button className='rest-btn'><span>30</span>s</button>
+                    <button className='rest-btn'><span>40</span>s</button>
+                    <button className='rest-btn'><span>60</span>s</button>
+                </div>
+                <hr />
+
+                <div className="calculate-time">
+                    <h2>Total Time</h2>
+                    <p className='exercise-time'>Exercise Time: <span>00</span>s</p>
+                    <p className='exercise-time'>Rest Time: <span>00</span>s</p>
+                </div>
+
+                <div className="toast">
+                    <button className='toast-btn'>Activity Completed</button>
+                </div>
+            </div>
         </div>
     );
 };
